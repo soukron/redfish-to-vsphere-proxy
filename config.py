b06d24e18ae8b04139f40b487f38b1dd141114d8
Sergio G <soukron@gmbros.net>
2025-05-07 23:35:11 +0200

    Modified config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 vsphere = {
-    'host': 'localhost',
+    'host': 'vcenter.gmbros.local',
     'vms': [
-        {'port': 3000, 'vmId': 'vm-4284'},
-        {'port': 3001, 'vmId': 'vm-4285'},
-        {'port': 3002, 'vmId': 'vm-4286'},
-        {'port': 3003, 'vmId': 'vm-4287'},
-        {'port': 3004, 'vmId': 'vm-4288'}
+        {'port': 3000, 'vmId': 'vm-4296'},
+        {'port': 3001, 'vmId': 'vm-4297'},
+        {'port': 3002, 'vmId': 'vm-4298'},
+        {'port': 3003, 'vmId': 'vm-4299'},
+        {'port': 3004, 'vmId': 'vm-4300'}
     ]
 }
