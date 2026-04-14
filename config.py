29c34de36f6085aca1ded126e219a6ff4c5ae919
Juancar Tovar <tovar@redhat.com>
2026-04-14 13:01:18 +0100

    local changes

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 vsphere = {
     'host': 'vcenter.gmbros.local',
     'vms': [
-        {'port': 3000, 'vmId': 'vm-4296'},
-        {'port': 3001, 'vmId': 'vm-4297'},
-        {'port': 3002, 'vmId': 'vm-4298'},
-        {'port': 3003, 'vmId': 'vm-4299'},
-        {'port': 3004, 'vmId': 'vm-4300'}
+        {'port': 3000, 'vmId': 'vm-5028'}
     ]
 }
